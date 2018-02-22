ff1fe9c0c0510c9ef6f72fc3261faacfeb0fe2cd
fernandosg <fsg_tmp@hotmail.com>
2018-02-22 00:49:34 -0600

    Refactoring. Using a new model, and testing with GLTFLoader instead OBJLoader

--- js/app.js
+++ js/app.js
@@ -1,32 +1,34 @@
 function App(){
 
 }
-
 App.prototype.start=function(){
   var geometry=new THREE.PlaneGeometry(1,1);
-
-  var loader = new THREE.ObjectLoader();
-  var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+  var ambientLight = new THREE.AmbientLight( 0xcccccc, 1.4 );
   framework.realidadEscena.escena.add(ambientLight);
   var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
   framework.realidadEscena.camara.add(pointLight);
-  loader.load(
-    "./source/SPACESHIP.json",
-    function ( obj ) {
-      console.dir(obj);
-      var spaceship=framework.createElement({WIDTH:60,HEIGHT:60,GEOMETRY:geometry});
-      spaceship.init();
-      spaceship.get().add(obj);
-      framework.addMarker({id:0,callback:this.spaceshipAction,puntero:spaceship,path:"patt.hiro"});
-    },
-    function ( xhr ) {
-    },
-    function ( err ) {
-    }
-  );
+
+        var loader = new THREE.GLTFLoader();
+      	loader.load(
+      		'./models/gnome.gltf',
+      		function ( gltf ) {
+            var gnome=framework.createElement({WIDTH:60,HEIGHT:60,GEOMETRY:geometry});
+            gnome.init();
+            gnome.get().add(gltf.scene);
+            framework.addMarker({id:0,callback:this.gnomeAction,puntero:gnome,path:"patt.hiro"});
+      		},
+      		function ( xhr ) {
+      			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+      		},
+      		function ( error ) {
+      			console.log( 'Error' );
+
+      		}
+      	);
 }
 
-App.prototype.spaceshipAction=function(puntero){
+App.prototype.gnomeAction=function(puntero){
   console.dir(puntero);
 }
 
